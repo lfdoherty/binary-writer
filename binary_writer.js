dfee10175c9cb2a93b89993970e9a5b4c4b22a69
Liam <lfdoherty@gmail.com>
2016-11-11 10:31:00 +0800

    commit

--- binary_writer.js
+++ binary_writer.js
@@ -20,23 +20,37 @@ class BinaryWriter {
 			this._truncateLastBuf()
 			this.pos = 0
 			this.buf = new Buffer(nextBufSize);
+			this.allBufs.push(this.buf)
 		}
 	}
 
 	putNumber(n: number){
 		this._ensure(8)
+	//	console.log('wrote number: ' + n)
 		this.buf.writeDoubleLE(n, this.pos)
 		this.pos += 8
 	}
 	putFloat(n: number){
 		this._ensure(4)
+	//	console.log('wrote float: ' + n)
 		this.buf.writeFloatLE(n, this.pos)
 		this.pos += 4
 	}
 	putInt(n: number){
+	//	console.log('put int: ' + n)
 		this._ensure(4)
 		this.buf.writeInt32LE(n, this.pos)
+	//	console.log('bytes: ')
+	//	console.log(this.buf.slice(this.pos, this.pos+4))
+	//	console.log(this.buf)
 		this.pos += 4
+	}
+	putString(str, encoding){
+		const len = Buffer.byteLength(str, encoding)
+		this.putInt(len);
+		this._ensure(len);
+		this.buf.write(str, this.pos, len, encoding)
+		this.pos += len;
 	}
 	putBuffer(buf){
 		if (buf instanceof ArrayBuffer) buf = Buffer.from(buf);
